571a2b96b66b408a919ade994b87bb511337e8d0
Deng Parek <dengparek@users.noreply.github.com@users.noreply.github.com>
2026-09-08 14:21:25 +0200

    feat: add environment configuration

---
+++ backend/src/config/env.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+export const NODE_ENV = process.env.NODE_ENV || "development";
+
+export const PORT = process.env.PORT || "5000";
+
+export const MONGO_URI = process.env.MONGO_URI;
+
+export const JWT_SECRET = process.env.JWT_SECRET;
+
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "1d";
