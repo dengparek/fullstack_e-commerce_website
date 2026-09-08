a7166f5230e9d7237dab2c50adb649b7762f4fab
Deng Parek <dengparek@users.noreply.github.com@users.noreply.github.com>
2026-09-08 16:49:54 +0200

    feat: export database schemas

---
+++ backend/src/database/schema/index.ts
@@ -0,0 +1,8 @@
+import { refreshTokens } from "./refresh_tokens";
+
+export * from "./users";
+export * from "./products";
+export * from "./carts";
+export * from "./orders";
+export * from "./refresh_tokens";
+export * from "./reviews";
