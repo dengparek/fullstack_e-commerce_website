7ccf53693ec9773d595db1909c1e84063051bb07
Deng Parek <dengparek@users.noreply.github.com@users.noreply.github.com>
2026-09-08 14:23:03 +0200

    included database connections

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+import type { Request, Response } from "express";
+
 import productRoutes from "./routes/productRoutes";
 import orderRoutes from "./routes/orderRoutes";
 import authRoutes from "./routes/authRoutes";
 import { urlencoded } from "express";
+import { connectDB } from "./database/db";
+import { NODE_ENV, PORT } from "./config/env";
 
 export const app = express();
 
@@ -10,12 +16,28 @@ export const app = express();
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 
-const port = 3000;
-
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/orders", orderRoutes);
 app.use("/api/v1/auth", authRoutes);
 
-app.listen(port, () => {
-  console.log(`App listening on port: ${port}`);
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "API is healthy",
+  });
 });
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        `Server started successfully on port:${PORT} in ${NODE_ENV} mode`,
+      );
+
+      console.log(`Server running at port:${PORT} in ${NODE_ENV} mode`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB: " + err.message);
+  });
+
+export default app;
