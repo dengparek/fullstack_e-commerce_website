b658d8ed8405185c985cae1ce2512693dcb0dbfa
Deng Parek <dengparek@users.noreply.github.com@users.noreply.github.com>
2026-09-08 12:58:50 +0200

    remove deleted files

--- README.md
+++
@@ -1,2 +0,0 @@
-# E-COMMERCE_BACKEND
-Ecommerce Backend API built with Node.js, Express, TypeScript, and Drizzle ORM. Provides a scalable, modular architecture with user auth, products, orders, cart, and secure API endpoints. Designed for clean code, performance, and easy integration with frontend apps.
--- src/controllers/auth.controller.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from "express";
-
-export const CreateUser = (req: Request, res: Response) => {
-  res.json("User created successfully");
-};
-
-export const LoginUser = (req: Request, res: Response) => {};
-
-export const SignOutUser = (req: Request, res: Response) => {};
