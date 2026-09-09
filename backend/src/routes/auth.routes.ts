5585ae2376f0323a82ff8b65300f65f7bc8424b5
Deng Parek <dengparek@users.noreply.github.com@users.noreply.github.com>
2026-09-09 13:02:14 +0200

    feat: add refresh token route

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { login, register } from "../controllers/auth.controller";
+import { login, register, refresh } from "../controllers/auth.controller";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.post("/refresh", refresh);
 
 export default authRouter;
