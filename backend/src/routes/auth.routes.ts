6cc782abcdfc4d061cfab5ef0c7dc706971f7d18
Deng Parek <dengparek@users.noreply.github.com@users.noreply.github.com>
2026-09-09 15:44:58 +0200

    feat: add logout route

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 
-import { login, register, refresh } from "../controllers/auth.controller";
+import {
+  login,
+  register,
+  refresh,
+  logout,
+} from "../controllers/auth.controller";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/refresh", refresh);
+authRouter.post("/logout", logout);
 
 export default authRouter;
